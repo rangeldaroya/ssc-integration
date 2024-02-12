740152af6c35a856fbe7f954d8dbc82e389a5b6d
rdaroya_umass_edu <rdaroya_umass_edu@login2.unity.rc.umass.edu>
2024-02-12 16:06:27 +0000

    Add small updates to matching and evaluating on opera

--- 10_script_eval_opera.sh
+++ 10_script_eval_opera.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem=4G  # Requested Memory
 #SBATCH -p gypsum-1080ti  # Partition
 #SBATCH -G 1  # Number of GPUs
-#SBATCH -o 10_slurm_opera/segnet-eval-%j.out  # %j = job ID
-#SBATCH --job-name=segnet-eval-opera
-#SBATCH --time=04:00:00 
+#SBATCH -o 10_slurm_opera/deeplabv3p-eval-%j.out  # %j = job ID
+#SBATCH --job-name=deeplabv3p-eval-opera
+#SBATCH --time=48:00:00 
 #SBATCH --mail-type=ALL
 
 
-python 10_opera_eval_model.py --backbone "segnet"
+python 10_opera_eval_model.py --backbone "deeplabv3p" --batch_size 1
